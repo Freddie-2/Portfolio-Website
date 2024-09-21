58c2e7425644dbe6e8c779685428fb6930af0d44
Fredrick Njoki <njokifredrick1@gmail.com>
2024-09-21 20:02:50 +0300

    Second update

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='SocialMedia'>
+            <LinkedInIcon/>
+        </div>
+        <p>&copy: fredrick.com</p>
+    </div>
+  )
+}
+
+export default Footer
--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import GithubIcon from '@mui/icons-material/GitHub';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className='home'>
+      <div className='about'>
+        <h2>Hi, My Name is Fredrick</h2>
+        <div className='prompt'>
+          <p>
+            A software developer with a passion for learning and creating.</p>
+            <LinkedInIcon/>
+            <EmailIcon/>
+            <GithubIcon/>
+        </div>
+      </div>
+      <div className='skills'>
+        <h1>Skills</h1>
+        <ol className='list'>
+          <li className='item'>
+            <h2>Front-End</h2>
+            <span>
+              ReactJS, HTML, CSS
+            </span>
+          </li>
+          <li className='item'>
+            <h2>Back-End</h2>
+            <span>
+              NodeJS, MySQL, Django
+            </span>
+          </li>
+          <li className='item'>
+            <h2>Languages</h2>
+            <span>
+              JavaScript, Python, Go
+            </span>
+          </li>
+        </ol>
+      </div>
     </div>
   )
 }
